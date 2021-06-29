32b22c0a190786d7dc3c3c072f6f76856bca528e
yashwanthmiryala <55680268+yashwanthmiryala@users.noreply.github.com>
2021-06-29 12:33:30 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(window).load(function(){
+
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-thumbnail',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on( 'click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  });
+
+})
+
